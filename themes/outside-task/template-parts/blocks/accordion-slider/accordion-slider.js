42dc985281e62800f0b9eb030ba6f093e7c6aba0
rafikzz <rafikmaharjan@gmail.com>
2024-07-24 08:01:46 +0545

    Added working picture and added pause play functionality

--- themes/outside-task/template-parts/blocks/accordion-slider/accordion-slider.js
+++ themes/outside-task/template-parts/blocks/accordion-slider/accordion-slider.js
@@ -3,43 +3,26 @@ document.addEventListener("DOMContentLoaded", function () {
     .querySelectorAll(".video-swiper-container")
     .forEach((container, index) => {
       const mySwiper = new Swiper(container.querySelector(".video-swiper"), {
-        autoplay: {
-          delay: 8000,
-        },
-        loop: true,
+        autoplay: true,
+        loop: false,
+        spaceBetween:20,
         autoWidth: true,
         slidesPerView: 1,
         on: {
           transitionStart: function () {
-            const videos = container.querySelectorAll("video");
-            const iframes = container.querySelectorAll("iframe");
-            videos.forEach((video) => video.pause());
-            iframes.forEach((iframe) => {
-              const src = iframe.src;
-              if (src.includes("vimeo.com")) {
-                const player = new Vimeo.Player(iframe);
-                player.pause();
-              }
-            });
+            pauseAllVideos(container);
           },
           transitionEnd: function () {
-            
-            const activeIndex = this.activeIndex;
-            const activeSlide =
+            let activeIndex = this.activeIndex;
+            let activeSlide =
               container.querySelectorAll(".swiper-slide")[activeIndex];
-            const activeSlideVideo = activeSlide.querySelector("video");
-            const activeSlideIframe = activeSlide.querySelector("iframe");
-            if (activeSlideVideo) {
-              activeSlideVideo.play();
-            }
-            if (activeSlideIframe) {
-              const src = activeSlideIframe.src;
-              if (src.includes("vimeo.com")) {
-                const player = new Vimeo.Player(activeSlideIframe);
-                player.play();
+              let playOrPauseButton = container.querySelector(".pause-button-js");
+              if(playOrPauseButton && !playOrPauseButton.classList.contains('paused'))
+              {
+                playVideo(activeSlide);
               }
-            }
           },
+         
           autoplayTimeLeft(s, time, progress) {
             container
               .querySelector(".go-to-buttons ")
@@ -53,13 +36,20 @@ document.addEventListener("DOMContentLoaded", function () {
           .querySelectorAll(".go-to-buttons .slider-accordion-item")
           .forEach((button) => {
             button.classList.remove("active");
+            button.classList.remove("playing");
             button.setAttribute("aria-expanded", "false");
           });
-        let parentCOntainer = container.querySelector(
+        let currentButton = container.querySelector(
           `.go-to-buttons button[data-slide="${index}"]`
-        ).parentElement;
-        parentCOntainer.classList.add("active");
-        parentCOntainer.setAttribute("aria-expanded", "true");
+        );
+        if (currentButton) {
+          let parentCOntainer = currentButton.parentElement;
+          if (parentCOntainer) {
+            parentCOntainer.classList.add("active");
+            parentCOntainer.classList.add("playing");
+            parentCOntainer.setAttribute("aria-expanded", "true");
+          }
+        }
       }
       // Initial active button setup
       updateActiveButton(mySwiper.realIndex);
@@ -67,15 +57,33 @@ document.addEventListener("DOMContentLoaded", function () {
       container.querySelectorAll(".go-to-buttons button").forEach((button) => {
         button.addEventListener("click", function () {
           const slideIndex = this.getAttribute("data-slide");
-          if(slideIndex){
-          mySwiper.slideTo(slideIndex);
-        }
+          if (slideIndex) {
+            mySwiper.slideTo(slideIndex);
+          }
         });
       });
       // Update active button on slide change
       mySwiper.on("slideChange", function () {
         updateActiveButton(mySwiper.realIndex);
       });
+      let playOrPauseButton = container.querySelector(".pause-button-js");
+      if (playOrPauseButton) {
+        playOrPauseButton.addEventListener("click", function () {
+          if (!playOrPauseButton.classList.contains("paused")) {
+            playOrPauseButton.classList.add("paused");
+            mySwiper.autoplay.stop();
+            pauseAllVideos(container);
+          } else {
+            var timeLeft =mySwiper.autoplay.timeLeft;
+            mySwiper.autoplay.start();
+            mySwiper.autoplay.timeLeft =timeLeft;
+            playOrPauseButton.classList.remove("paused");
+            let activeSlide =
+              container.querySelectorAll(".swiper-slide")[mySwiper.activeIndex];
+            playVideo(activeSlide);
+          }
+        });
+      }
     });
   //For Accordian
   function initializeAccordion(accordionElement) {
@@ -90,9 +98,14 @@ document.addEventListener("DOMContentLoaded", function () {
           .querySelectorAll(".slider-accordion-item")
           .forEach((item) => {
             item.classList.remove("active");
+            item.classList.remove("playing");
+
             item.setAttribute("aria-expanded", "false");
           });
+        this.closest(".go-to-buttons ").style.setProperty("--progress", 0);
         this.closest(".slider-accordion-item").classList.add("active");
+        this.closest(".slider-accordion-item").classList.add("playing");
+
         this.closest(".slider-accordion-item").setAttribute(
           "aria-expanded",
           "true"
@@ -104,3 +117,44 @@ document.addEventListener("DOMContentLoaded", function () {
   const accordions = document.querySelectorAll(".slider-accordion");
   accordions.forEach(initializeAccordion);
 });
+
+const pauseAllVideos = (container) => {
+  const videos = container.querySelectorAll("video");
+  const iframes = container.querySelectorAll("iframe");
+  videos.forEach((video) => video.pause());
+  iframes.forEach((iframe) => {
+    const src = iframe.src;
+    if (src.includes("vimeo.com")) {
+      const player = new Vimeo.Player(iframe);
+      player.pause();
+    }
+  });
+};
+const playVideo = (activeSlide) => {
+  const activeSlideVideo = activeSlide.querySelector("video");
+  const activeSlideIframe = activeSlide.querySelector("iframe");
+  if (activeSlideVideo) {
+    activeSlideVideo.play();
+  }
+  if (activeSlideIframe) {
+    const src = activeSlideIframe.src;
+    if (src.includes("vimeo.com")) {
+      const player = new Vimeo.Player(activeSlideIframe);
+      player.play();
+    }
+  }
+};
+const pauseVideo = (activeSlide) => {
+  const activeSlideVideo = activeSlide.querySelector("video");
+  const activeSlideIframe = activeSlide.querySelector("iframe");
+  if (activeSlideVideo) {
+    activeSlideVideo.pause();
+  }
+  if (activeSlideIframe) {
+    const src = activeSlideIframe.src;
+    if (src.includes("vimeo.com")) {
+      const player = new Vimeo.Player(activeSlideIframe);
+      player.pause();
+    }
+  }
+};
--- themes/outside-task/template-parts/blocks/accordion-slider/accordion-slider.php
+++ themes/outside-task/template-parts/blocks/accordion-slider/accordion-slider.php
@@ -30,89 +30,83 @@ $footer_title            = get_field('footer_title');
 
 <div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
     <div class="video-tab">
-        <div class="container">
-
-            <div class=" video-swiper-container">
-                <div class="video-swiper__row">
-                    <?php if ($accordian && is_array($accordian)) :  ?>
-                        <div class="video-swiper__col-video ">
-                            <div class="video-swiper ">
-                                <div class="swiper-wrapper ">
-                                    <?php foreach ($accordian as $accordian_item) :  ?>
-                                        <?php if (isset($accordian_item['video'])) :  ?>
-                                            <div class="swiper-slide" <?php echo (isset($accordian_item['duration']) && is_numeric($accordian_item['duration'])) ? 'data-swiper-autoplay="' . $accordian_item['duration'] . '"' : ''; ?>>
-                                                <div class="video-container">
-                                                    <?php if (strpos($accordian_item['video'], 'vimeo.com') !== false) : ?>
-                                                        <iframe src="<?php echo esc_url($accordian_item['video']); ?>?background=1" frameborder="0" allow="autoplay"></iframe>
-                                                    <?php else : ?>
-                                                        <video src="<?php echo esc_url($accordian_item['video']); ?>" poster="<?php echo get_template_directory_uri() . '/assets/img/image.png'  ?> " preload="none" muted></video>
-                                                    <?php endif; ?>
-                                                </div>
+        <div class=" video-swiper-container">
+            <div class="video-swiper__row">
+                <?php if ($accordian && is_array($accordian)) :  ?>
+                    <div class="video-swiper__col-video ">
+                        <div class="video-swiper ">
+                            <div class="swiper-wrapper ">
+                                <?php foreach ($accordian as $accordian_item) :  ?>
+                                    <?php if (isset($accordian_item['video'])) :  ?>
+                                        <div class="swiper-slide" <?php echo (isset($accordian_item['duration']) && is_numeric($accordian_item['duration'])) ? 'data-swiper-autoplay="' . $accordian_item['duration'] . '"' : ''; ?>>
+                                            <div class="video-container">
+                                                <?php if (strpos($accordian_item['video'], 'vimeo.com') !== false) : ?>
+                                                    <iframe src="<?php echo esc_url($accordian_item['video']); ?>?background=1" frameborder="0" allow="autoplay"></iframe>
+                                                <?php else : ?>
+                                                    <video src="<?php echo esc_url($accordian_item['video']); ?>" poster="<?php echo get_template_directory_uri() . '/assets/img/image.png'  ?> " preload="none" muted></video>
+                                                <?php endif; ?>
                                             </div>
-                                        <?php endif; ?>
-                                    <?php endforeach; ?>
-                                </div>
+                                        </div>
+                                    <?php endif; ?>
+                                <?php endforeach; ?>
                             </div>
                         </div>
-                    <?php endif; ?>
+                        <button class="video-swiper__col-pause-button pause-button-js"></button>
+                    </div>
+                <?php endif; ?>
 
-                    <div class="video-swiper__col-accordian ">
-                        <div class="video-swiper__col__container">
-                            <div class="accordian-container">
-                                <?php if ($section_title) :  ?>
-                                    <div class="video-tab__title">
-                                        <?php echo $section_title; ?>
-                                    </div>
-                                <?php endif; ?>
-                                <?php
-                                $count = 0;
-                                if ($accordian && is_array($accordian)) :
-                                ?>
-                                    <?php if (isset($accordian_item['video'])) :  ?>
-                                        <div class="slider-accordion go-to-buttons">
-                                            <?php foreach ($accordian as $accordian_item) :
-                                                $title = isset($accordian_item['title']) ? $accordian_item['title'] : false;
-                                                $content = isset($accordian_item['content']) ? $accordian_item['content'] : false;
-                                            ?>
-                                                <div class="slider-accordion-item <?php echo $count == 0 ? 'active' : ''   ?>  ">
-                                                    <?php if ($title) :
-
-                                                    ?>
-                                                        <button class="slider-accordion-header" data-slide="<?php echo $count; ?>"><?php echo esc_attr($title); ?></button>
-                                                    <?php endif; ?>
-                                                    <div class="slider-accordion-content">
-                                                        <?php if ($content) :  ?>
-                                                            <p><?php echo esc_attr($content); ?></p>
-                                                        <?php endif; ?>
-
-                                                    </div>
-                                                </div>
-                                            <?php
-                                                $count++;
-                                            endforeach; ?>
-                                        </div>
-                                <?php endif;
-                                endif; ?>
-                            </div>
-                            <?php if ($footer_content || $footer_title) :  ?>
-                                <div class="slider-accordion__commont-note">
-                                    <?php if ($footer_title) :  ?>
-                                        <div class="slider-accordion__commont-note__title">
-                                            <?php echo esc_attr($footer_title); ?>
-                                        </div>
-                                    <?php endif; ?>
-                                    <?php if ($footer_content) :  ?>
-                                        <p>
-
-                                            <?php echo esc_attr($footer_content); ?>
-                                        </p>
-
-                                    <?php endif; ?>
-
+                <div class="video-swiper__col-accordian ">
+                    <div class="video-swiper__col__container">
+                        <div class="accordian-container">
+                            <?php if ($section_title) :  ?>
+                                <div class="video-tab__title">
+                                    <?php echo $section_title; ?>
                                 </div>
                             <?php endif; ?>
-                        </div>
+                            <?php
+                            $count = 0;
+                            if ($accordian && is_array($accordian)) :
+                            ?>
+                                <?php if (isset($accordian_item['video'])) :  ?>
+                                    <div class="slider-accordion go-to-buttons">
+                                        <?php foreach ($accordian as $accordian_item) :
+                                            $title = isset($accordian_item['title']) ? $accordian_item['title'] : false;
+                                            $content = isset($accordian_item['content']) ? $accordian_item['content'] : false;
+                                        ?>
+                                            <div class="slider-accordion-item <?php echo $count == 0 ? 'active' : ''   ?>  ">
+                                                <?php if ($title) :
 
+                                                ?>
+                                                    <button class="slider-accordion-header" data-slide="<?php echo $count; ?>"><?php echo esc_attr($title); ?></button>
+                                                <?php endif; ?>
+                                                <div class="slider-accordion-content">
+                                                    <?php if ($content) :  ?>
+                                                        <p><?php echo esc_attr($content); ?></p>
+                                                    <?php endif; ?>
+
+                                                </div>
+                                            </div>
+                                        <?php
+                                            $count++;
+                                        endforeach; ?>
+                                    </div>
+                            <?php endif;
+                            endif; ?>
+                        </div>
+                        <?php if ($footer_content || $footer_title) :  ?>
+                            <div class="slider-accordion__commont-note">
+                                <?php if ($footer_title) :  ?>
+                                    <div class="slider-accordion__commont-note__title">
+                                        <?php echo esc_attr($footer_title); ?>
+                                    </div>
+                                <?php endif; ?>
+                                <?php if ($footer_content) :  ?>
+                                    <p>
+                                        <?php echo esc_attr($footer_content); ?>
+                                    </p>
+                                <?php endif; ?>
+                            </div>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
